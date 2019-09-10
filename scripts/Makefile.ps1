489e3cbd658ef60643a368a45a8921dacc09837a
Guillermo Vaya <guivaya@gmail.com>
2019-09-10 15:55:07 +0200

    ignore chocolatey

--- scripts/Makefile.ps1
+++ scripts/Makefile.ps1
@@ -361,7 +361,11 @@ function Install-Deps {
             }
             "wix" {
                 Print-Info "Installing wixtoolset..."
-                choco install wixtoolset --yes
+                # choco install wixtoolset --yes
+                $WebClient = New-Object System.Net.WebClient
+                $WebClient.DownloadFile("https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311.exe","wix311.exe")
+                # todo: check hash
+                wix311.exe -q
                 break;
             }
             "signtool" {
