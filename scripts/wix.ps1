b005f94e9338bf0e733830b829aa361a9c792b83
Guillermo Vaya <guivaya@gmail.com>
2019-09-10 17:12:20 +0200

    fix typo

--- scripts/wix.ps1
+++ scripts/wix.ps1
@@ -22,7 +22,7 @@ Print-Info "Downloading wixtoolset..."
 # choco install wixtoolset --yes
 $WebClient = New-Object System.Net.WebClient
 #$WebClient.DownloadFile("https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311.exe",".\scripts\wix.exe")
-$WebClient.DownloadFile("https://github.com/wixtoolset/wix3/releases/download/wix3104rtm/wix310.exe",".\scripts\wix.exe"
+$WebClient.DownloadFile("https://github.com/wixtoolset/wix3/releases/download/wix3104rtm/wix310.exe",".\scripts\wix.exe")
 Print-Info "Installing wixtoolset..."
 # todo: check hash
 .\scripts\wix.exe -q
